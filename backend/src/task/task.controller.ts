7412fb35c108c3853c140fc129d8b9ac7c888b20
fidojahfal <fidojahfalprayoga@gmail.com>
2025-10-17 02:33:34 +0700

    feat: add new get task by id to get specific task

--- backend/src/task/task.controller.ts
+++ backend/src/task/task.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { Auth } from '../common/decorator/auth.decorator';
 import { type Login } from '@prisma/client';
 import { TaskCreateRequest, TaskResponse } from '../model/task.model';
@@ -17,6 +25,19 @@ export class TaskController {
     console.log(request);
 
     const result = await this.taskService.create(user, request);
+
+    return {
+      data: result,
+    };
+  }
+
+  @Get('/:task_id')
+  @HttpCode(200)
+  async getTask(
+    @Auth() user: Login,
+    @Param('task_id', ParseIntPipe) task_id: number,
+  ): Promise<WebResponse<TaskResponse>> {
+    const result = await this.taskService.getTask(user, task_id);
 
     return {
       data: result,
--- backend/src/task/task.service.ts
+++ backend/src/task/task.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../common/prisma/prisma.service';
 import { TaskCreateRequest, TaskResponse } from '../model/task.model';
-import { Login } from '@prisma/client';
+import { Login, Task } from '@prisma/client';
 import { ValidationService } from '../common/validation/validation.service';
 import { TaskValidation } from './task.validation';
 
@@ -11,6 +11,18 @@ export class TaskService {
     private prismaService: PrismaService,
     private validationService: ValidationService,
   ) {}
+
+  private toTaskResponse(task: Task): TaskResponse {
+    return {
+      task_id: task.task_id,
+      user_id: task.user_id,
+      title: task.title,
+      description: task.description,
+      status: task.status,
+      deadline: task.deadline,
+      created_by: task.created_by,
+    };
+  }
 
   async create(user: Login, request: TaskCreateRequest): Promise<TaskResponse> {
     const createRequest: TaskCreateRequest = this.validationService.validate(
@@ -32,14 +44,20 @@ export class TaskService {
       data: { ...createRequest, created_by: user.user_id },
     });
 
-    return {
-      task_id: task.task_id,
-      user_id: task.user_id,
-      title: task.title,
-      description: task.description,
-      status: task.status,
-      deadline: task.deadline,
-      created_by: task.created_by,
-    };
+    return this.toTaskResponse(task);
+  }
+
+  async getTask(user: Login, task_id: number): Promise<TaskResponse> {
+    const task = await this.prismaService.task.findUnique({
+      where: {
+        task_id,
+      },
+    });
+
+    if (!task) {
+      throw new HttpException('Task not found', 404);
+    }
+
+    return this.toTaskResponse(task);
   }
 }
