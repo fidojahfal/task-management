b88f926f03f12d106e5af6939b57e775d54c10c4
fidojahfal <fidojahfalprayoga@gmail.com>
2025-10-16 23:25:20 +0700

    feat: create initialization file for task feature

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
 import { AuthModule } from './auth/auth.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [CommonModule, AuthModule],
+  imports: [CommonModule, AuthModule, TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
