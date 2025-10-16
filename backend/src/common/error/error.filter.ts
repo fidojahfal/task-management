692e9b647d53e30fa62ed0a7ab17928508d2808b
fidojahfal <fidojahfalprayoga@gmail.com>
2025-10-17 02:32:10 +0700

    feat: add error jwt to error filter to make sure jwt error not breaking the server

--- backend/src/common/error/error.filter.ts
+++ backend/src/common/error/error.filter.ts
@@ -4,9 +4,10 @@ import {
   ExceptionFilter,
   HttpException,
 } from '@nestjs/common';
+import { JsonWebTokenError } from '@nestjs/jwt';
 import { ZodError } from 'zod';
 
-@Catch(ZodError, HttpException)
+@Catch(ZodError, HttpException, JsonWebTokenError)
 export class ErrorFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
     const response = host.switchToHttp().getResponse();
@@ -18,6 +19,10 @@ export class ErrorFilter implements ExceptionFilter {
     } else if (exception instanceof ZodError) {
       response.status(403).json({
         errors: 'Validation error!',
+      });
+    } else if (exception instanceof JsonWebTokenError) {
+      response.status(401).json({
+        errors: exception.message,
       });
     } else {
       response.status(500).json({
