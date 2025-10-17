84c136bd406f280ca8e8917efff1c03f163bd69c
fidojahfal <fidojahfalprayoga@gmail.com>
2025-10-17 08:13:06 +0700

    feat: add change minimum of password input in backend to be minimum 8 characters

--- backend/src/auth/auth.validation.ts
+++ backend/src/auth/auth.validation.ts
@@ -4,11 +4,11 @@ export class AuthValidation {
   static readonly REGISTER = z.object({
     name: z.string().min(1).max(100),
     username: z.string().min(1).max(100),
-    password: z.string().min(1).max(255),
+    password: z.string().min(8).max(255),
   });
 
   static readonly LOGIN = z.object({
     username: z.string().min(1).max(100),
-    password: z.string().min(1).max(255),
+    password: z.string().min(8).max(255),
   });
 }
