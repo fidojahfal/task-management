a3822bc60693b582fdb35c0f925d48219bc7af76
fidojahfal <fidojahfalprayoga@gmail.com>
2025-10-17 10:24:06 +0700

    feat: create middleware to protect routes from unauthorized user

---
+++ frontend/middleware.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+
+const AUTH_PATH = ["/login", "/register"];
+
+export default async function middleware(request) {
+  try {
+    const token = request.cookies.get("token");
+    const { pathname } = request.nextUrl;
+
+    if (!token && !AUTH_PATH.includes(pathname)) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    if(AUTH_PATH.includes(pathname)){
+        return NextResponse.redirect(new URL("/task", request.url));
+    }
+
+    return NextResponse.next();
+  } catch (error) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/login", "/register", "/task/:path*"],
+};
